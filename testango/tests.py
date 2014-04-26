6928829799dd3d5f0a3c61bf41e9dbdad798ec13
Pablo Martín <goinnn@gmail.com>
2014-04-26 18:24:13 +0200

    Adding the test

--- testango/tests.py
+++ testango/tests.py
@@ -1,3 +1,16 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test.client import Client
 
-# Create your tests here.
+
+class TestangoTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=False)
+
+    def testango_word(self):
+        word = 'hello'
+        url = reverse('word_view', args=(word,))
+        response = self.client.get(url)
+        self.assertEqual(response.content, word)
+        self.assertNotEqual(response.content, 'bye')
